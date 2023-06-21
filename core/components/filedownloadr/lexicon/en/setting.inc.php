ba130519a5e8e7039fb30902f391f05cd59eb9bc
Thomas Jakobi <buero@treehillstudio.de>
2023-06-21 14:29:51 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MODX Extras/MODX FileDownloadR/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-filedownloadr-system-settings/en/

--- core/components/filedownloadr/lexicon/en/setting.inc.php
+++ core/components/filedownloadr/lexicon/en/setting.inc.php
@@ -9,7 +9,7 @@ $_lang['area_email'] = 'Email';
 $_lang['area_geolocation'] = 'Geolocation';
 $_lang['setting_filedownloadr.email_props'] = 'Email Properties';
 $_lang['setting_filedownloadr.email_props_desc'] = 'JSON encoded array of email properties for the FileDownloadEmail plugin.';
-$_lang['setting_filedownloadr.exclude_scan'] = 'Exclude files';
+$_lang['setting_filedownloadr.exclude_scan'] = 'Exclude Files';
 $_lang['setting_filedownloadr.exclude_scan_desc'] = 'Comma-separated list of filenames to be excluded during scanning.';
 $_lang['setting_filedownloadr.ipinfodb_api_key'] = 'API Key of IPInfoDB.com';
 $_lang['setting_filedownloadr.ipinfodb_api_key_desc'] = 'Get the API Key from <a href="https://ipinfodb.com/" target="_blank">https://ipinfodb.com/</a>.';
