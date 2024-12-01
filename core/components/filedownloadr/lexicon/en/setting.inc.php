27ab49f9da34cf908d9e28dab85990a1f591d43f
Thomas Jakobi <thomas.jakobi@partout.info>
2024-12-01 15:55:51 +0100

    Update lexicon for 3.2.0

--- core/components/filedownloadr/lexicon/en/setting.inc.php
+++ core/components/filedownloadr/lexicon/en/setting.inc.php
@@ -8,10 +8,12 @@
 $_lang['area_email'] = 'Email';
 $_lang['area_geolocation'] = 'Geolocation';
 $_lang['setting_filedownloadr.email_props'] = 'Email Properties';
-$_lang['setting_filedownloadr.email_props_desc'] = 'JSON encoded array of email properties for the FileDownloadEmail plugin.';
+$_lang['setting_filedownloadr.email_props_desc'] = 'JSON-encoded array of email properties for the FileDownloadEmail plugin.';
 $_lang['setting_filedownloadr.exclude_scan'] = 'Exclude Files';
 $_lang['setting_filedownloadr.exclude_scan_desc'] = 'Comma-separated list of filenames to be excluded during scanning.';
 $_lang['setting_filedownloadr.ipinfodb_api_key'] = 'API Key of IPInfoDB.com';
 $_lang['setting_filedownloadr.ipinfodb_api_key_desc'] = 'Get the API Key from <a href="https://ipinfodb.com/" target="_blank">https://ipinfodb.com/</a>.';
 $_lang['setting_filedownloadr.use_geolocation'] = 'Use Geolocation by IP';
 $_lang['setting_filedownloadr.use_geolocation_desc'] = 'Use geolocation by IPInfoDB.com.';
+$_lang['setting_filedownloadr.extended_file_fields'] = 'Extended File Fields';
+$_lang['setting_filedownloadr.extended_file_fields_desc'] = 'JSON-encoded array of extended file fields. The array can use the keys ‘name’ and ‘type’.';
