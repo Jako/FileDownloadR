10cd7169c2ab541fdec9f87f30662ed9ba69d14b
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:06 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/filedownloadr/lexicon/es/default.inc.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Default Lexicon Entries for FileDownloadR
+ *
+ * @package filedownloadr
+ * @subpackage lexicon
+ */
+$_lang['filedownloadr'] = '';
+$_lang['filedownloadr.breadcrumb.home'] = '';
+$_lang['filedownloadr.counter_err_save'] = '';
+$_lang['filedownloadr.delete'] = '';
+$_lang['filedownloadr.dir_count'] = '';
+$_lang['filedownloadr.dir_count_not'] = '';
+$_lang['filedownloadr.dir_count_user'] = '';
+$_lang['filedownloadr.dir_count_user_not'] = '';
+$_lang['filedownloadr.dir_counts'] = '';
+$_lang['filedownloadr.dir_counts_not'] = '';
+$_lang['filedownloadr.dir_counts_user'] = '';
+$_lang['filedownloadr.dir_counts_user_not'] = '';
+$_lang['filedownloadr.directories'] = '';
+$_lang['filedownloadr.file_count'] = '';
+$_lang['filedownloadr.file_count_user'] = '';
+$_lang['filedownloadr.file_counts'] = '';
+$_lang['filedownloadr.file_counts_user'] = '';
+$_lang['filedownloadr.file_date'] = '';
+$_lang['filedownloadr.file_err_delete'] = '';
+$_lang['filedownloadr.file_err_not_exist'] = '';
+$_lang['filedownloadr.file_name'] = '';
+$_lang['filedownloadr.file_size'] = '';
+$_lang['filedownloadr.path'] = '';
+$_lang['filedownloadr.remote_err_file_empty'] = '';
+$_lang['filedownloadr.remote_err_file_not_available'] = '';
+$_lang['filedownloadr.upload_button'] = '';
+$_lang['filedownloadr.upload_err_count'] = '';
+$_lang['filedownloadr.upload_err_empty'] = '';
+$_lang['filedownloadr.upload_err_exists'] = '';
+$_lang['filedownloadr.upload_err_filesize'] = '';
+$_lang['filedownloadr.upload_err_not_allowed'] = '';
+$_lang['filedownloadr.upload_err_not_writable'] = '';
+$_lang['filedownloadr.upload_text'] = '';
